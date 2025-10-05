e22e52a018a4ef9ffc806982068aa7ab5ce4c90e
lailaporter55 <lmp3003@rit.edu>
2025-10-05 16:00:54 -0400

    html response

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -4,17 +4,19 @@ const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 const css = fs.readFileSync(`${__dirname}/../client/styles.css`);
 
 const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end();
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(index);
+    response.end();
 };
+
 
 const getCSS = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/css' });
-  response.write(css);
-  response.end();
-};
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.write(css);
+    response.end();
+}
 
 module.exports = {
-  getIndex,
+    getIndex,
+    getCSS,
 };
