9f6b61259e374a512dad1db102e383850368a16f
lailaporter55 <lmp3003@rit.edu>
2025-10-05 09:22:41 -0400

    working on json

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,3 +1,5 @@
+//for the milestone checkpoint i am using books and making sure everything works
+//before i decide if i want to change my dataset 
 const books = {};
 
 const respondJSON = (request, response, status, object) => {
@@ -17,4 +19,26 @@ const getBooks = (request, response) => {
         books,
     };
     respondJSON(request, response, 200, responseJSON);
+}
+
+//add more information here
+//if the user wants to add a book, they need to provide a title and an author
+const addBook = (request, response, body) => {
+    const responseJSON = {
+        message: 'Title and author are both required.',
+    };
+
+    if(!body.title || !body.author) {
+        responseJSON.id = 'missingParams';
+        return respondJSON(request, response, 400, responseJSON);
+    }
+
+    let status = 204;
+    const{ title, author, read = false } = request.body;
+    if(books[body.title]) {
+        status = 204;
+        books[title]= {};
+}
+    books[title] = title; 
+    books[author] = author;
 }
