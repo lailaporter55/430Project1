b9f2b5f760ca350aec649a9f57978ff7ee04dc06
lailaporter55 <lmp3003@rit.edu>
2025-10-14 21:08:47 -0400

    rating and fixing getbooks

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -13,22 +13,52 @@ const respondJSON = (request, response, status, object) => {
     }
     response.end();
 };
-
-const getBooks = (request, response) => {
+//get books by author or title
+//if no params return all books
+//GET and HEAD requests
+const getBooks = (request, response, params = {}) => {
     const responseJSON = {
-        books,
+        books: [],
     };
-    respondJSON(request, response, 200, responseJSON);
-}
+    //if params return books that match author or title
+    if(params.title) {
+        const book = books[params.title];
+        if(book) {
+            responseJSON.books.push(book);
+            return respondJSON(request, response, 200, responseJSON);
+        }
+        else {
+            responseJSON.message = 'No books found with that title.';
+            responseJSON.id = 'notFound';
+            return respondJSON(request, response, 404, responseJSON);
+        }
+    }
+    if(params.author) {
+        const authorBooks = Object.values(books).filter((book) => book.author === params.author);
+        if(authorBooks.length > 0) {
+            responseJSON.books = authorBooks;
+            return respondJSON(request, response, 200, responseJSON);
+        }
+        else {
+            responseJSON.message = 'No books found by that author.';
+            responseJSON.id = 'notFound';
+            return respondJSON(request, response, 404, responseJSON);
+        }
+    }
+    //if params are not recognized, return all books
+    //if no params return all books
+    responseJSON.books = Object.values(books);
+    return respondJSON(request, response, 200, responseJSON);
+};
 
 //add a book
 //I will either have this function add a book that is not in the system 
 //or it will be to add a book to the users cart 
-const addBook = (request, response, body) => {
+const requestBook = (request, response, body) => {
     const responseJSON = {
         message: 'Title and author are both required.',
     };
-
+    //check to make sure required fields are there
     if(!body.title || !body.author) {
         responseJSON.id = 'missingParams';
         return respondJSON(request, response, 400, responseJSON);
@@ -51,40 +81,52 @@ const addBook = (request, response, body) => {
     return respondJSON(request, response, status);
 }; 
 
-const checkOutBook = (request, response, body) => {
-    const responseJSON = {
-        message: 'Title and author are both required.',
-    };
 
-    if(!body.title || !body.author) {
-        responseJSON.id = 'missingParams';
+//i am removing the checkout book function, as well as return books 
+
+//ratebook function
+//this will be a POST request that
+//takes a book title 
+//user rates 1-5 
+const rateBook = (request, response, body) => {
+    //check to make sure required fields are there
+    if(!body.title || !body.rating) {
+        const responseJSON = {
+            message: 'Title and rating are both required.',
+            id: 'missingParams',
+        };
         return respondJSON(request, response, 400, responseJSON);
     }
-
-    let status = 204;
-    const{ title, author, read = false } = request.body;
-    if(books[body.title]) {
-        status = 204;
-        books[title]= {};
-}
-    books[title] = title; 
-    books[author] = author;
-
-    if(status === 201) {
-        responseJSON.message = 'Added to Cart!';
-        respondJSON(request, response, status, responseJSON);
-        return respondJSON(request, response, status, responseJSON);
+    //check that book exists
+    if(!books[body.title]) {
+        const responseJSON = {
+            message: 'The book you are trying to rate does not exist',
+            id: 'notFound',
+        };
+        return respondJSON(request, response, 404, responseJSON);
     }
-    return respondJSON(request, response, status);
-};
-//since there would be a checkout book there would need to be
-//remove a book from cart function as well
-
-//if the book is read by the user the user will be able to rate the book 
-//this will be a POST request 
+    //check that rating is between 1 and 5
+    if (body.rating < 1 || body.rating > 5) {
+        const responseJSON = {
+            message: 'Rating must be between 1 and 5',
+            id: 'badRating',
+        };
+        return respondJSON(request, response, 400, responseJSON);
+    }
+    //add rating to book
+    if (body.rating >= 1 || body.rating <= 5) {
+        books[body.title].rating = body.rating;
+        const responseJSON = {
+            message: 'Rating added successfully',
+        };
+        return respondJSON(request, response, 200, responseJSON);
+    }
+    
+}; 
 
 module.exports = {
     getBooks,
-    addBook,
-    checkOutBook,
+    requestBook, 
+    rateBook
+    
 };
