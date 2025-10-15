674fb5ba534230e508bdbd8c078020326d77d98c
lailaporter55 <lmp3003@rit.edu>
2025-10-14 22:09:55 -0400

    cliet script

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/index.html`);
 const css = fs.readFileSync(`${__dirname}/../client/styles.css`);
+const notFoundPage = fs.readFileSync(`${__dirname}/../client/notFound.html`);
 
 const getIndex = (request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -16,7 +17,14 @@ const getCSS = (request, response) => {
     response.end();
 }
 
+const getnotFound = (request, response) => {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.write('<h1>404 Not Found</h1>');
+    response.end();
+};
+
 module.exports = {
     getIndex,
     getCSS,
+    getnotFound,
 };
